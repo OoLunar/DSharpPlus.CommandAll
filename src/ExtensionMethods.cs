9bc527356af453b14c19f2d9523aafe52ec71fc1
Lunar Starstrum <lunar@forsaken-borders.net>
2023-07-04 17:34:25 -0500

    Remove janky logging solution

--- src/ExtensionMethods.cs
+++ src/ExtensionMethods.cs
@@ -32,46 +32,14 @@ namespace DSharpPlus.CommandAll
                 throw new InvalidOperationException("CommandAll Extension is already initialized.");
             }
 
-            configuration ??= new(new ServiceCollection());
+            configuration ??= new();
             ServiceDescriptor? currentLoggingImplementation = configuration.ServiceCollection.FirstOrDefault(service => service.ServiceType == typeof(ILoggerFactory));
-
-            // There are 4 scenarios here:
-            // - The user does not provide a logging implementation.
-            // - The user provides a logging implementation only to the DiscordClient.
-            // - The user provides a the default ShardedLoggerFactory implementation through the services
-            // - The user provides a custom logging implementation through the services
 
             // No implementation provided
             if (currentLoggingImplementation is null)
             {
-                // Check if the client has a valid logging implementation
-                Type clientType = client.Logger.GetType();
-                if (clientType != _shardedLoggerFactoryType)
-                {
-                    Type[] clientInterfaces = clientType.GetInterfaces();
-                    if (clientInterfaces.Any(@interface => @interface.IsGenericType && @interface.GetGenericTypeDefinition() == typeof(ILogger<>)))
-                    {
-                        configuration.ServiceCollection.AddSingleton(typeof(ILogger<>), client.Logger);
-                    }
-
-                    if (clientInterfaces.Contains(typeof(ILoggerFactory)))
-                    {
-                        configuration.ServiceCollection.AddSingleton(typeof(ILoggerFactory), client.Logger);
-                    }
-                }
-                else
-                {
-                    Console.WriteLine($"No logging system set, using a {nameof(NullLoggerFactory)}. This is not recommended, please provide a logging system so you can see errors.");
-                    configuration.ServiceCollection.AddSingleton<ILoggerFactory, NullLoggerFactory>().AddSingleton(typeof(ILogger<>), typeof(NullLogger<>));
-                }
-            }
-            // Check if they provided the ShardedLoggerFactory explicitly to the services
-            else if (currentLoggingImplementation.ServiceType == _shardedLoggerFactoryType)
-            {
-                Console.WriteLine($"ShardedLoggerFactory detected, using {nameof(NullLoggerFactory)} instead. CommandAll is NOT compatible with the default logging system that DSharpPlus provides!");
-                configuration.ServiceCollection
-                    .RemoveAll<ILoggerFactory>().RemoveAll(typeof(ILogger<>)) // Remove the default logging implementation, if set
-                    .AddSingleton<ILoggerFactory, NullLoggerFactory>().AddSingleton(typeof(ILogger<>), typeof(NullLogger<>));
+                Console.WriteLine($"No logging system set, using a {nameof(NullLoggerFactory)}. This is not recommended, please provide a logging system so you can see errors.");
+                configuration.ServiceCollection.AddSingleton<ILoggerFactory, NullLoggerFactory>().AddSingleton(typeof(ILogger<>), typeof(NullLogger<>));
             }
 
             CommandAllExtension extension = new(configuration);
@@ -92,20 +60,13 @@ namespace DSharpPlus.CommandAll
             }
 
             await shardedClient.InitializeShardsAsync();
-            configuration ??= new(new ServiceCollection());
+            configuration ??= new();
 
             ServiceDescriptor? currentLoggingImplementation = configuration.ServiceCollection.FirstOrDefault(service => service.ServiceType == typeof(ILoggerFactory));
             if (currentLoggingImplementation is null)
             {
                 Console.WriteLine($"No logging system set, using a {nameof(NullLoggerFactory)}. This is not recommended, please provide a logging system so you can see errors.");
                 configuration.ServiceCollection.AddSingleton<ILoggerFactory, NullLoggerFactory>().AddSingleton(typeof(ILogger<>), typeof(NullLogger<>));
-            }
-            else if (currentLoggingImplementation.ServiceType == _shardedLoggerFactoryType && shardedClient.Logger.GetType() == _shardedLoggerFactoryType)
-            {
-                Console.WriteLine($"ShardedLoggerFactory detected, using {nameof(NullLoggerFactory)} instead. CommandAll is NOT compatible with the default logging system that DSharpPlus provides!");
-                configuration.ServiceCollection
-                    .RemoveAll<ILoggerFactory>().RemoveAll(typeof(ILogger<>)) // Remove the default logging implementation, if set
-                    .AddSingleton<ILoggerFactory, NullLoggerFactory>().AddSingleton(typeof(ILogger<>), typeof(NullLogger<>));
             }
 
             Dictionary<int, CommandAllExtension> extensions = new();
