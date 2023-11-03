183f29b6824eb30c5b357773b1805a9ba5c6d84f
Lunar Starstrum <lunar@forsaken-borders.net>
2023-11-03 11:58:28 -0500

    Attempt to fix text parsing

--- examples/DSharpPlus.CommandAll.Basics/Commands/EchoCommand.cs
+++ examples/DSharpPlus.CommandAll.Basics/Commands/EchoCommand.cs
@@ -1,12 +1,13 @@
 using System.Threading.Tasks;
 using DSharpPlus.CommandAll.Commands;
 using DSharpPlus.CommandAll.Commands.Attributes;
+using DSharpPlus.CommandAll.Processors.TextCommands.Attributes;
 
 namespace DSharpPlus.CommandAll.Examples.Basics.Commands
 {
     public sealed class EchoCommand
     {
         [Command("echo")]
-        public static async Task ExecuteAsync(CommandContext context, string text1, string? text2 = null, string? text3 = null, string? text4 = null, string? text5 = null) => await context.RespondAsync($"{text1} {text2} {text3} {text4} {text5}".Trim());
+        public static async Task ExecuteAsync(CommandContext context, string text1, [RemainingText] string? text2 = null) => await context.RespondAsync($"{text1} {text2}".Trim());
     }
 }
