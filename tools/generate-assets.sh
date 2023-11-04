b1ca62484487cccc4969b4dbf9fe4a5aa2d648f8
InF <inftord@inftord.tech>
2023-11-05 00:14:36 +0500

    Update DocFX and use a different template (#4)
    Yep.
    
    ---------
    
    Co-authored-by: Lunar Starstrum <lunar@forsaken-borders.net>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- tools/generate-assets.sh
+++ tools/generate-assets.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-# Deps
-apt-get install ImageMagick yarn > /dev/null
-yarn global add svgo > /dev/null
+# If svgo isn't installed, install it
+if ! command -v svgo &> /dev/null; then
+  # Check if yarn is installed
+  if ! command -v yarn &> /dev/null; then
+    if ! command -v apt-get &> /dev/null; then
+      echo "Unable to install svgo. Please install svgo or yarn manually."
+      exit 1
+    fi
+    sudo apt-get install -y yarn > /dev/null
+  fi
+  yarn global add svgo > /dev/null
+fi
 
-# Functions
+# Function to regenerate assets
 regenerate()
 {
   echo "Generating assets for $1"
@@ -13,13 +22,12 @@ regenerate()
   svgo --multipass --quiet "$1"
 
   # Small size for DocFX
-  convert -background none -resize 64x64 "$1" "${1%.*}_small.png"
+  convert -background none -resize 48x48 "$1" "${1%.*}_small.png"
 
   # Convert to PNG
   convert -background none "$1" "${1%.*}.png"
 
   # Convert to ICO
-  # https://stackoverflow.com/a/15104985
   convert -background transparent -define "icon:auto-resize=16,24,32,64,128,256" "$1" "${1%.*}.ico"
 }
 
@@ -29,14 +37,18 @@ for file in res/*.svg; do
     regenerate "$file"
 done
 
+# Copy all resource files into the images directory
+cp res/*.{svg,png,ico} docs/images/
+
+# Uncomment the following lines if you want to automatically commit and push changes to Git
 # Check if any files were modified
-git config --global user.email "github-actions[bot]@users.noreply.github.com"
-git config --global user.name "github-actions[bot]"
-git add res > /dev/null
-git diff-index --quiet HEAD
-if [ "$?" == "1" ]; then
-  git commit -m "[ci-skip] Regenerate resource files." > /dev/null
-  git push > /dev/null
-else
-  echo "No resource files were modified."
-fi
+#git config --global user.email "github-actions[bot]@users.noreply.github.com"
+#git config --global user.name "github-actions[bot]"
+#git add res > /dev/null
+#git diff-index --quiet HEAD
+#if [ "$?" == "1" ]; then
+#  git commit -m "[ci-skip] Regenerate resource files." > /dev/null
+#  git push > /dev/null
+#else
+#  echo "No resource files were modified."
+#fi
