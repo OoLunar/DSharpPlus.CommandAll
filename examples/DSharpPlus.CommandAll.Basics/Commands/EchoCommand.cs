72883f5a16f926fb660cc9aa6086c8bef268f949
Lunar Starstrum <lunar@forsaken-borders.net>
2023-11-12 11:31:43 -0600

    Gross hack for interactions and params

--- examples/DSharpPlus.CommandAll.Basics/Commands/EchoCommand.cs
+++ examples/DSharpPlus.CommandAll.Basics/Commands/EchoCommand.cs
@@ -1,13 +1,12 @@
 using System.Threading.Tasks;
 using DSharpPlus.CommandAll.Commands;
 using DSharpPlus.CommandAll.Commands.Attributes;
-using DSharpPlus.CommandAll.Processors.TextCommands.Attributes;
 
 namespace DSharpPlus.CommandAll.Examples.Basics.Commands
 {
     public sealed class EchoCommand
     {
         [Command("echo")]
-        public static async Task ExecuteAsync(CommandContext context, [RemainingText] string text) => await context.RespondAsync(text);
+        public static async Task ExecuteAsync(CommandContext context, params string[] text) => await context.RespondAsync(string.Join(' ', text));
     }
 }
